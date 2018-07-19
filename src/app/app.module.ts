bab27309ce7c8d05065ca51c0cf0a689363fc228
Jqjwjejrjtjy <willlucas1512@gmail.com>
2018-07-19 12:49:41 -0300

    +adicionando os arquivos

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {MaterializeModule} from 'angular2-materialize';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PokemonsComponent } from './pages/pokemons/pokemons.component';
+import { HomeComponent } from './pages/home/home.component';
+import { BerriesComponent } from './pages/berries/berries.component';
+import { AppRoutingModule } from './app.routing.module'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    PokemonsComponent,
+    HomeComponent,
+    BerriesComponent
+  ],
+  imports: [
+    BrowserModule,
+    MaterializeModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
